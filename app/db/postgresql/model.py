79d2222a8d6bf9f9785e5bcb265e15942302a739
juxxon23 <juxxon23@hotmail.com>
2024-05-05 22:57:03 -0500

    formatting functions added

--- app/db/postgresql/model.py
+++ app/db/postgresql/model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,7 +13,7 @@ class User(db.Model):
     email = db.Column(db.String(40), nullable=False)  
     phone_number = db.Column(db.String(20), nullable=False)
     address = db.Column(db.String(40), nullable=False)
-    employee_id = db.Column(db.String(10), nullable=False)
+    employee_id = db.Column(db.String(10), nullable=False) # Cambiar employee_id por personal_id
     dateof_hire = db.Column(db.DateTime, nullable=False)
     job_title = db.Column(db.String(30), nullable=False)
     
@@ -26,3 +27,20 @@ class User(db.Model):
         self.employee_id = employee_id
         self.dateof_hire = dateof_hire
         self.job_title = job_title
+    
+    def as_dict_format(self):
+        # Formatear la fecha de contratación en el formato "day/month/year"
+        dateof_hire_formatted = self.dateof_hire.strftime("%d/%m/%Y")
+        return {
+            "address": self.address,
+            "dateofHire": dateof_hire_formatted,
+            "email": self.email,
+            "personalID": self.employee_id,
+            "firstName": self.first_name,
+            "jobTitle": self.job_title,
+            "lastName": self.last_name,
+            "phoneNumber": self.phone_number
+        }
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
