1729c3ec301354bdfcc24b6187eee3e39141f53e
juxxon23 <juxxon23@hotmail.com>
2024-05-05 22:53:33 -0500

    cors config modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from flask_cors import CORS
 from .db.postgresql.model import db
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True) # (instance_relative_config) rutas relativas a la carpeta de la instancia, instance_path (default)
     app.config.from_object('config')
     app.config.from_pyfile('config.py')
-    CORS(app, support_credentials=True)
+    #CORS(app, resources={r"/*": {"origins": "*"}}, allow_headers=["Content-Type", "Authorization", "idUser"], methods=["GET", "PUT", "POST", "DELETE"], supports_credentials=True)
     
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,idUser')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
+
     with app.app_context():
         from .routes import user
         add_routes(app, user)
@@ -17,3 +23,4 @@ def create_app():
 def add_routes(app, user):
     # User routes
     app.add_url_rule(user['employee'], view_func=user['view_func_employee'])
+    app.add_url_rule(user['allemployee'], view_func=user['view_func_allemployee'])
